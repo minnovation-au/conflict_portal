4760483dc1e4fc25eed484f7918c1eb38c980541
minnovation-au <support@minnovation.com.au>
2016-11-29 09:15:48 +1100

    Create conflict.js

---
+++ js/conflict.js
@@ -0,0 +1,89 @@
+/**
+
+The script file for scripts used in the Conflict Plugin.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2015 M-innovation Australia
+
+
+ */
+
+var acc = document.getElementsByClassName('accordion');var i;for (i = 0; i < acc.length; i++) {acc[i].onclick = function(){this.classList.toggle('active');this.nextElementSibling.classList.toggle('show');}}
+
+// <![CDATA[
+ function submit_form(){
+ 	if(typeof(validateCaptchaAndSubmit)!='undefined'){
+ 		validateCaptchaAndSubmit();
+ 	}else{
+ 		check_webtolead_fields();
+ 	}
+ }
+ function check_webtolead_fields(){
+     if(document.getElementById('bool_id') != null){
+        var reqs=document.getElementById('bool_id').value;
+        bools = reqs.substring(0,reqs.lastIndexOf(';'));
+        var bool_fields = new Array();
+        var bool_fields = bools.split(';');
+        nbr_fields = bool_fields.length;
+        for(var i=0;i<nbr_fields;i++){
+          if(document.getElementById(bool_fields[i]).value == 'on'){
+             document.getElementById(bool_fields[i]).value = 1;
+          }
+          else{
+             document.getElementById(bool_fields[i]).value = 0;
+          }
+        }
+      }
+    if(document.getElementById('req_id') != null){
+        var reqs=document.getElementById('req_id').value;
+        reqs = reqs.substring(0,reqs.lastIndexOf(';'));
+        var req_fields = new Array();
+        var req_fields = reqs.split(';');
+        nbr_fields = req_fields.length;
+        var req = true;
+        for(var i=0;i<nbr_fields;i++){
+          if(document.getElementById(req_fields[i]).value.length <=0 || document.getElementById(req_fields[i]).value==0){
+           req = false;
+           break;
+          }
+        }
+        if(req){
+            document.WebToLeadForm.submit();
+            return true;
+        }
+        else{
+          alert('Please provide all the required fields');
+          return false;
+         }
+        return false
+   }
+   else{
+    document.WebToLeadForm.submit();
+   }
+}
+function validateEmailAdd(){
+	if(document.getElementById('email1') && document.getElementById('email1').value.length >0) {
+		if(document.getElementById('email1').value.match(/^\w+(['\.\-\+]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/) == null){
+		  alert('Not a valid email address');
+		}
+	}
+	if(document.getElementById('email2') && document.getElementById('email2').value.length >0) {
+		if(document.getElementById('email2').value.match(/^\w+(['\.\-\+]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/) == null){
+		  alert('Not a valid email address');
+		}
+	}
+}
+// ]]>
